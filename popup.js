3b31fa0219e194c2bb89af0b0f50510234ddcbb1
hugojay <hugojay@gmail.com>
2013-02-02 22:23:59 +0800

    Useless commit
    因為 chrome extension manifest v2 規定不得跨站 http 的 script 請求，故此程式失效

--- popup.js
+++ popup.js
@@ -12,9 +12,12 @@ $(document).ready(function(){
 			success: function(result){
 				if(result){
 					$("body").addClass("result");
-					var start = result.indexOf("<!-- ##################主要內容################# BEGIN -->");
-					var end = result.indexOf("<!-- ##################主要內容################# END -->");
-					result = process(result.substring(start, end));
+					var head = result.match(/<head.*>/),
+					    headstart = result.indexOf(head) + head.toString().length,
+					    headend = result.indexOf("</head>"),
+					    start = result.indexOf("<!-- ##################主要內容################# BEGIN -->"),
+					    end = result.indexOf("<!-- ##################主要內容################# END -->");
+					result = process(result.substring(headstart, headend)) + result.substring(start, end);
 					$("#content").html(result);
 				}else{
 					$("#content").html("LOAD FAILED");
@@ -29,16 +32,19 @@ $(document).ready(function(){
 });
 
 function process(result){
-	//var patt = /<script>str_trim\('.*?('\);*<\/script>)/g;
-	var patt1 = "<script>str_trim\\('";
-	var patt2 = "\\);*<\\/script>"; // ');</script>
-	var patt = new RegExp(patt1 + ".*?(" + patt2 + ")", "g");
+	// <script src="/webpost/js/common.js"></script>
+	//var patt = /<script .*src=".*".*><\/script>/g;
+	var patt1 = "<script .*src=\"";
+	var patt2 = "\"><\\/script>";
+	var patt = new RegExp(patt1 + ".*" + patt2, "g");
 	var matches = result.match(patt);
 	var splities = result.split(patt);
 	if(matches != null){
 		for(var i in matches){
-			matches[i] = matches[i].replace(new RegExp(patt1, "g"), "").replace(new RegExp(patt2, "g"), "");
-			splities[i] = splities[i] + matches[i];
+			var start = matches[i].match(new RegExp(patt1)).toString().length,
+				end = matches[i].indexOf(matches[i].match(new RegExp(patt2))[0]),
+				url = "http://postserv.post.gov.tw/" + matches[i].substring(start, end);
+			$.getScript(url);
 		}
 		return splities.join("");
 	}else
